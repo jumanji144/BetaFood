86288732de699730a96e598fb6c4c5db2704132b
Justus Garbe <gihihoh@gmail.com>
2023-05-20 11:45:16 +0200

    Fix blacklist not correctly applying

--- src/main/java/me/darknet/betafood/client/mixin/MixinLivingEntity.java
+++ src/main/java/me/darknet/betafood/client/mixin/MixinLivingEntity.java
@@ -2,11 +2,11 @@ package me.darknet.betafood.client.mixin;
 
 import me.darknet.betafood.BetaFood;
 import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
@@ -20,14 +20,11 @@ public abstract class MixinLivingEntity {
 					target="Lnet/minecraft/world/entity/LivingEntity;spawnItemParticles(Lnet/minecraft/world/item/ItemStack;I)V"
 			))
 	private void onTriggerItemUseEffects(LivingEntity livingEntity, ItemStack itemStack, int i) {
-		if(!(livingEntity instanceof Player)) { // only do it on players
-			livingEntity.spawnItemParticles(itemStack, i);
-		} else {
-			if (!itemStack.isEdible() || !BetaFood.getConfig().isBlacklisted(
-					BuiltInRegistries.ITEM.getKey(itemStack.getItem()).toString())) {
-						livingEntity.spawnItemParticles(itemStack, i);
-					}
+		if(livingEntity instanceof Player) {
+			ResourceLocation itemID = BuiltInRegistries.ITEM.getKey(itemStack.getItem());
+			if(!BetaFood.getConfig().isBlacklisted(itemID.toString())) return;
 		}
+		livingEntity.spawnItemParticles(itemStack, i);
 	}
 
 }
